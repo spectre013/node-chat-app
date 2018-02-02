e1d826b7440dce9354307e44df8e10fde54abc6a
Brian Paulson <brian.paulson@polarisalpha.com>
2018-02-02 14:53:11 -0700

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const publicPath = path.join(__dirname,'..','public');
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+app.use(bodyParser.json());
+app.use(express.static(publicPath));
+
+
+
+
+app.listen(port, () => {
+    console.log(`Servers is up on port ${port}`)
+});
